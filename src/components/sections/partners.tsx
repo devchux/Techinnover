b3374965f2a62928052a8bd43f657d0079911494
Chukwudi Eze <chukwudieze97@gmail.com>
2024-01-25 09:57:47 +0100

    implement fanbase

---
+++ src/components/sections/partners.tsx
@@ -0,0 +1,24 @@
+import "./partners.scss";
+
+const Partners = () => {
+  return (
+    <section className="partners">
+      <h3>
+        JOIN TOP ARTISTS, BUILD YOUR <span>FANBASE</span>
+      </h3>
+      <p>
+        Figma files can also be downloaded from our store. These files include
+        the latest Figma features and are synced
+      </p>
+      <div className="images">
+        <img src="/images/fanbase/1.png" alt="" />
+        <img src="/images/fanbase/2.png" alt="" />
+        <img src="/images/fanbase/3.png" alt="" />
+        <img src="/images/fanbase/4.png" alt="" />
+        <img src="/images/fanbase/5.png" alt="" />
+      </div>
+    </section>
+  );
+};
+
+export default Partners;
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,6 +2,7 @@ import Career from "../components/sections/career";
 import Header from "../components/sections/header";
 import Moves from "../components/sections/moves";
 import Navbar from "../components/sections/navbar";
+import Partners from "../components/sections/partners";
 import Tems from "../components/sections/tems";
 
 function Home() {
@@ -12,6 +13,7 @@ function Home() {
       <Career />
       <Moves />
       <Tems />
+      <Partners />
     </main>
   );
 }
